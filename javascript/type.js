5f5d67a498e4fd7c383723f58ff157400bad3ec8
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-05-19 01:07:40 +0900

    js types

---
+++ javascript/type.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(typeof true);// => "boolean"
+console.log(typeof 42); // => "number"
+console.log(typeof .05) // => "number"
+console.log(typeof "JavaScript"); // => "string"
+console.log(typeof Symbol("シンボル"));// => "symbol"
+console.log(typeof undefined); // => "undefined"
+console.log(typeof null); // => "object"
+console.log(typeof ["配列"]); // => "object"
+console.log(typeof { "key": "value" }); // => "object"
+console.log(typeof function() {}); // => "function"
